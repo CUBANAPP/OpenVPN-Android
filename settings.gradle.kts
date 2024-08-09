f28b6e71ca016daa81633e267e39154f727e1851
Predatorzgz <Predatorzgz@gmail.com>
2024-08-08 21:57:36 -0400

    AAR compiled
    Deleted the temp copy of :main (:openvpn) for clean

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,25 +1,31 @@
-/*
- * Copyright (c) 2012-2016 Arne Schwabe
- * Distributed under the GNU GPL v2 with additional terms. For full terms see the file doc/LICENSE.txt
- */
 pluginManagement {
     repositories {
-        gradlePluginPortal()
-        google()
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
         mavenCentral()
-
+        gradlePluginPortal()
+        mavenLocal()
+        maven ( "https://repo.spring.io/plugins-release" )
+        maven ( "https://jitpack.io" )
+        maven ( "https://plugins.gradle.org/m2/" )
     }
 }
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
+        mavenLocal()
         mavenCentral()
-        maven(url = "https://jitpack.io")
+        maven ( "https://jitpack.io" )
     }
 }
 
+rootProject.name = "OpenVPN-Android"
 include(":main")
 include(":tlsexternalcertprovider")
 include(":remoteExample")
-//include(":yubikeyplugin")
